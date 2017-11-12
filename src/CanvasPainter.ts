d3a48d16a7bb52158f37742fe7ca29bb9f810800
Christopher Ronning <christopher.a.ronning@gmail.com>
2017-11-12 13:33:02 -0500

    diffuse gradient

--- src/CanvasPainter.ts
+++ src/CanvasPainter.ts
@@ -7,8 +7,8 @@ import Camera from './Camera';
 
 export default class CanvasPainter {
   canvas: HTMLCanvasElement;
-  context: CanvasRenderingContext2D; // fix once this is better understood
-  ns: number; // no idea wtf this is supposed to be, fuck it
+  context: CanvasRenderingContext2D;
+  ns: number;
 
   constructor(canvas: HTMLCanvasElement, width: number, height: number) {
     this.canvas = canvas;
@@ -18,15 +18,28 @@ export default class CanvasPainter {
     this.context = this.canvas.getContext('2d')!;
   }
 
+  static randomInUnitSphere() {
+    let p: Vector;
+    do {
+      p = Vector.multiply(2.0, new Vector(Math.random(), Math.random(), Math.random()))
+        .subtract(new Vector(1, 1, 1));
+    } while (p.squaredLength() >= 1.0);
+    
+    return p;
+  }
+
   static color(ray: Ray, world: Hitable) : Vector {
-    const hit = world.hit(ray, 0.0, Number.MAX_VALUE, null);
+    const hit = world.hit(ray, 0.001, Number.MAX_VALUE, null);
+    
     if (hit) {
-      return Vector.multiply(
-        0.5, 
-        new Vector(hit.normal.x + 1.0, hit.normal.y + 1.0, hit.normal.z + 1.0));
+      const target: Vector = Vector.add(hit.p, hit.normal)
+        .add(CanvasPainter.randomInUnitSphere());
+      
+      return Vector.multiply(0.5, this.color(new Ray(hit.p, target.subtract(hit.p)), world));
     } else {
       const unitDirection: Vector = Vector.unitVector(ray.direction());
       const t: number = 0.5 * (unitDirection.y + 1.0);
+      
       return Vector.add(
         Vector.multiply(1.0 - t, new Vector(1.0, 1.0, 1.0)),
         Vector.multiply(t, new Vector(0.5, 0.7, 1.0))
@@ -44,9 +57,17 @@ export default class CanvasPainter {
     const vertical = new Vector(0.0, 2.0, 0.0);
     const origin = new Vector(0.0, 0.0, 0.0);
 
+    // example scene from the book
+    // const list: Array<Hitable> = [
+    //   new Sphere(new Vector(0, 0, -1.0), 0.5),
+    //   new Sphere(new Vector(0, -100.5, -1), 100)
+    // ]
+    
     const list: Array<Hitable> = [
-      new Sphere(new Vector(0, 0, -1.0), 0.5),
-      new Sphere(new Vector(0, -100.5, -1), 100)
+      new Sphere(new Vector(0, 0, -1.6), 0.85),
+      new Sphere(new Vector(0, -100.5, -1), 100),
+      new Sphere(new Vector(-1, 1.2, -2.2), 0.7),
+      new Sphere(new Vector(1, 1.2, -2.2), 0.7)
     ];
     
     const world = new HitableList(list);
@@ -55,7 +76,7 @@ export default class CanvasPainter {
     for (let j = this.canvas.height - 1; j >= 0; j--) {
       for (let i = 0; i < this.canvas.width; i++) {
 
-        const col: Vector = new Vector(0, 0, 0);
+        let col: Vector = new Vector(0, 0, 0);
         for (let s = 0; s < this.ns; s++) {
           const u: number = (i + Math.random()) / this.canvas.width;
           const v: number = (j + Math.random()) / this.canvas.height;
@@ -66,7 +87,7 @@ export default class CanvasPainter {
         }
 
         col.divide(this.ns);
-
+        col = new Vector(Math.sqrt(col.x), Math.sqrt(col.y), Math.sqrt(col.z));
         const ir = 255.99 * col.x;
         const ig = 255.99 * col.y;
         const ib = 255.99 * col.z;
